f5aad15098afb4e6949ec5321062db209ce107f9
Ignacio Verasay <iverasay@users.noreply.github.com>
2020-03-20 12:03:31 +0100

    Object domain layer
    I create a layer for to put all the bussines  objects abstractions.

---
+++ CubesSol/CubesSol.ObjectDomain/cube.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CubesSol.ObjectDomain
+{
+    public class cube
+    {
+        public int dimension { get; set; }
+        public int xCoord { get; set; }
+        public int yCoord { get; set; }
+        public int zCoord { get; set; }
+    }
+}
