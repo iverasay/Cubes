4b7e3899077972d4087b312d60811e02483f4d0c
Ignacio Verasay <iverasay@users.noreply.github.com>
2020-03-20 12:14:11 +0100

    Aplication Domain Layer
    Here we going to put all the bussines logic

---
+++ CubesSol/CubesSol.AplicationDomain/ADCube.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CubesSol.ObjectDomain;
+
+namespace CubesSol.AplicationDomain
+{
+    public class ADCube : IADCube
+    {
+        public bool thereIsIntersection(cube pCube1, cube pCube2)
+        {
+            // Here will evalueates if the cubes colliding or not, and gives back an answer.
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CubesSol/CubesSol.AplicationDomain/IADCube.cs
@@ -0,0 +1,14 @@
+﻿using CubesSol.ObjectDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CubesSol.AplicationDomain
+{
+    public interface IADCube
+    {
+        bool thereIsIntersection(cube pCube1, cube pCube2);
+    }
+}
