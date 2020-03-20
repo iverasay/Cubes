789c912a8adf5c8d9a6b832c7d1171e112826701
Ignacio Verasay <iverasay@users.noreply.github.com>
2020-03-20 11:56:15 +0100

    Start proyect
    First at all I create a  WebApi Project with unit test for to use it then

---
+++ CubesSol/CubesSol.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CubesSol.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CubesSol/CubesSol.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CubesSol.WebApi.WebApiApplication" Language="C#" %>
